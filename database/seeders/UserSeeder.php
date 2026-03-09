47ce747da6691b03e318fb7c09649ef4ef0dba14
MinhazDeveloper <minhazt14@gmail.com>
2026-03-09 19:30:11 +0600

    previous data delete

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,6 +14,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        User::query()->delete();
         // Admin
         User::create([
             'name' => 'Jhon',
